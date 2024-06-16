bc7b666b36d6cd77dd9a6e70ba53ab5e81df42f5
moa <larssonmoaml@gmail.com>
2024-06-16 16:35:40 +0200

    prerender ( true

--- src/routes/push/+page.ts
+++ src/routes/push/+page.ts
@@ -1,7 +1,3 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
-
 import { supabase } from '$lib/supabase';
 import type { PageLoad } from './$types';
 

5bc5c2a609477601a66ab72fbf7a1dccad9edf57
moa <larssonmoaml@gmail.com>
2024-06-16 16:38:04 +0200

    url check

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -7,15 +7,17 @@ export const GET = async (event) => {
 		url,
 		locals: { supabase }
 	} = event;
-	const code = url.searchParams.get('code') as string;
-	const next = url.searchParams.get('next') ?? '/';
-	if (code) {
-		const { error } = await supabase.auth.exchangeCodeForSession(code);
-		if (!error) {
-			throw redirect(303, `/${next.slice(1)}`);
+
+	if (url) {
+		const code = url.searchParams.get('code') as string;
+		const next = url.searchParams.get('next') ?? '/';
+		if (code) {
+			const { error } = await supabase.auth.exchangeCodeForSession(code);
+			if (!error) {
+				throw redirect(303, `/${next.slice(1)}`);
+			}
 		}
 	}
-
 	// return the user to an error page with instructions
 	throw redirect(303, '/auth/auth-code-error');
 };
